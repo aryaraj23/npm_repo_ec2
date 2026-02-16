586cd1cac89499becc5e5482d4500dcd53dd02c4
Arya Raj <aryaraj152@gmail.com>
2026-02-16 21:39:14 +0530

    Create variable.tf
    added the variables

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "aws_region{
+  type = string
+  description = "AWS region"
+}
+
+varable "ami_id" {
+  type = string
+  description = "EC2 AMI ID"
+}
+
+variable "instance_type" {
+  type = string
+  description = "EEC2 instance type"
+} 
+
+variable "instance_name" {
+  type = string
+  description = "Name of the EC2 Instance"
+}
