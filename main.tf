189326b11a87880a559e52f5a31e33e13aaec0c1
Arya Raj <aryaraj152@gmail.com>
2026-02-16 22:21:49 +0530

    Update main.tf
    updating main

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_instance" "npm_web" {
    ami = var.ami_id
    instance_type = var.instance_type
+   key_name               = var.key_name
+   associate_public_ip_address = true
+
+   vpc_security_group_ids = [aws_security_group.ssh_sg.id]
 
    tags = {
      Name = "var.instance_name"
