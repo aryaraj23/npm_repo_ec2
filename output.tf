a6420d1110bee0ed70cf5fcab538678cdd156b28
Arya Raj <aryaraj152@gmail.com>
2026-02-16 21:29:45 +0530

    Update output.tf
    need to get the instance and public ip of the instance

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
 output "instance_id" {
   description = "EC2 Instance ID"
-  value = aws_instance.web-npm.id
+  value = aws_instance.npm_web.id
+}
+
+output "public_ip" {
+  description = "EC2 instance public ID"
+  value = aws_instance.npm_web.public_id
 }
